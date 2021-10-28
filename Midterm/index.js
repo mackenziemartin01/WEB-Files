415823d0e9677b2bc87850cd152c40ae93042a15
mackenziemartin01 <89860565+mackenziemartin01@users.noreply.github.com>
2021-10-28 12:20:01 -0600

    midterm

---
+++ Midterm/index.js
@@ -0,0 +1,34 @@
+document.getElementById("Mondaybutton"),addEventListener('click',function(){
+  document.getElementById("mondayitems").classList.toggle("shown");
+}
+);
+
+document.getElementById("Tuesdaybutton"),addEventListener('click',function(){
+  document.getElementById("tuesdayitems").classList.toggle("shown");
+}
+);
+
+document.getElementById("Wednesdaybutton"),addEventListener('click',function(){
+  document.getElementById("wednesdayitems").classList.toggle("shown");
+}
+);
+
+document.getElementById("Thursdaybutton"),addEventListener('click',function(){
+  document.getElementById("thursdayitems").classList.toggle("shown");
+}
+);
+
+document.getElementById("Fridaybutton").addEventListener('click',function(){
+  document.getElementById("fridayitems").classList.toggle("shown");
+}
+);
+
+document.getElementById("Saturdaybutton"),addEventListener('click',function(){
+  document.getElementById("saturdayitems").classList.toggle("shown");
+}
+);
+
+document.getElementById("Sundaybutton"),addEventListener('click',function(){
+  document.getElementById("sundayitems").classList.toggle("shown");
+}
+);
